530d0c87f02abf52f22cfca15a278885d8f7f4ea
hackywit <hackywit@aliyun.com>
2017-09-01 10:45:54 +0800

    在.babelrc文件中添加对Ant UI组件css的自动获取，并在webpack配置文件添加对css解析的支持

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -7,9 +7,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     devtool: 'source-map',
-    entry: path.join(__dirname , "../src/index.jsx"),
+    entry: path.join(__dirname, "../src/index.jsx"),
     output: {
-        path: path.join(__dirname , "../dist"),
+        path: path.join(__dirname, "../dist"),
         filename: "./js/[hash].js"
     },
     devServer: {
@@ -26,13 +26,23 @@ module.exports = {
                     loader: "babel-loader"
                 },
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }
+                ]
             }
         ]
     },
     plugins: [
         new webpack.BannerPlugin('版权所有，翻版必究'),
         new HtmlWebpackPlugin({
-            template: path.join(__dirname , "../src/index_template.html"),
+            template: path.join(__dirname, "../src/index_template.html"),
             filename: './index.html'
         }),
         new ExtractTextPlugin("style.css")
