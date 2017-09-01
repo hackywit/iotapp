62c16aa364d057911b009daef2691a8872aa9b1a
hackywit <hackywit@aliyun.com>
2017-09-01 10:31:23 +0800

    补上漏掉的loader.jsx，这是页面逻辑的主要入口

---
+++ src/components/loader.jsx
@@ -0,0 +1,17 @@
+/**
+ * Created by hackywit on 2017/9/1.
+ */
+import React, {Component} from 'react'
+import { DatePicker } from 'antd';
+
+class Loader extends Component {
+    render() {
+        return (
+            <div>
+                <DatePicker />
+            </div>
+        );
+    }
+}
+
+export default Loader
